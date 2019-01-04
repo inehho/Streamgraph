ec4ca19d79c98bf7210b856128544dd411a881f2
inehho <40414778+inehho@users.noreply.github.com>
2019-01-04 00:31:58 -0600

    Adding changes to my java scrip file

---
+++ Project/Static/JS/graphtwo.js
@@ -0,0 +1,62 @@
+const CHART = document.getElementById("graphTwo");
+console.log(CHART);
+let graphTwo = new Chart(CHART, {
+    type: 'line',
+    data: {
+        labels: ["Jan", "FEB", "March", "April"],
+        datasets: [
+            {
+                label: "Hardcoded Dataset",
+                fill: false,
+                lineTension: 0.1,
+                backgroundColor: "rgba(255, 99, 132, 0.2)",
+                borderColor:"rgba(255, 159, 64, 0.2)",
+                borderCapStyle: 'round',
+                borderDash:[],
+                borderDashOffset:0.0,
+                borderJoinStyle: 'mitter',
+                pointBoderColor: "rgba(255, 159, 64, 0.2)",
+                pointerBackgroundColor: "#fff",
+                pointerBorderWidth: 1,
+                pointerHoverRadius: 5,
+                pointHoverBackgroundColor: "rgba(255, 156, 64, 0.2)",
+                pointHoverBorderColor: "rgba(220, 220, 220, 1)",
+                pointHoverBorderWidth:2,
+                pointRadius: 1,
+                pointHitRadius:10,
+                data: [55,34,22,44,36,]
+            },
+            {
+                label: "Hardcoded Dataset",
+                fill: true,
+                lineTension: 0.6, 
+                backgroundColor: "rgba(255, 110, 132, 0.2)",
+                borderColor:"rgba(255, 179, 64, 0.2)",
+                borderCapStyle: 'round',
+                borderDash:[],
+                borderDashOffset:0.0,
+                borderJoinStyle: 'mitter',
+                pointBoderColor: "rgba(255, 159, 64, 0.2)",
+                pointerBackgroundColor: "#fff",
+                pointerBorderWidth: 1,
+                pointerHoverRadius: 5,
+                pointHoverBackgroundColor: "rgba(255, 156, 64, 0.2)",
+                pointHoverBorderColor: "rgba(120, 120, 160, 1)",
+                pointHoverBorderWidth:2,
+                pointRadius: 1,
+                pointHitRadius:10,
+                data: [60,15,82,34,26,]
+            }
+        ]},
+        options: {
+            scales: {
+                yAxes:[{
+                    ticks: {
+                        begingAtZero:true
+                    }
+                }]            
+                 }
+            }
+
+        }
+);
